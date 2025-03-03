6cbf649b084bafc8c40c3ddd47942b182cf84e39
playoffthecuff <jmobile@bk.ru>
2025-03-03 06:37:53 +0300

    fix: unique slug validator

--- lib/isUniqueAcrossSameTypeDocs.ts
+++ lib/isUniqueAcrossSameTypeDocs.ts
@@ -1,15 +1,19 @@
 const isUniqueAcrossSameTypeDocs = async (slug: any, context: any) => {
-  const {getClient} = context
-  const client = getClient({apiVersion: '2024-02-27'})
+  const { getClient } = context
+  const client = getClient({ apiVersion: '2024-02-27' })
+
+  const docId = context.document?._id
+  const publishedId = docId?.startsWith('drafts.') ? docId.replace('drafts.', '') : docId
+  const draftId = `drafts.${publishedId}`
 
   const exists = await client.fetch(
-    `count(*[_type == $type && slug.current == $slug && !(_id in [$draft, $published])])`,
+    `count(*[_type == $type && slug.current == $slug && !(_id in [$draftId, $publishedId])])`,
     {
       type: context.document?._type,
       slug,
-      draft: `drafts.${context.document?._id}`,
-      published: context.document?._id,
-    },
+      draftId,
+      publishedId,
+    }
   )
 
   return exists === 0;
